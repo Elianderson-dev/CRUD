51ac2453b22a5c19ac98756b6cc0f57b6e8c1fff
elianderson-dev <nin2333124568@gmail.com>
2022-05-25 18:04:29 +0000

    feat: adds temporary user data

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 "use strict"
 
+
 const openModal = () => document.getElementById("modal")
     .classList.add("active")
 
 const closeModal = () => document.getElementById("modal")
     .classList.remove("active")
-	
+
+const tempClient = {
+	name: "Frodo" 
+	e-mail: "frodob@gmail.com"
+	phone-number: "89-744333531"
+	town: "Hobbiton"
+}
+// CRUD | Create Read Update Delete
+
 // events 
 document.getElementById("registerClient")
     .addEventListener("click", openModal)
